a6ff27ee3bdc68ac66ea8c6f37a62c61f345a5ad
Felipe Roque <roque@MacBook-Pro-de-Felipe.local>
2020-04-13 01:38:01 -0300

    alteracao no report

--- src/test/java/br/com/roque/integration/login/LoginCucumberTest.java
+++ src/test/java/br/com/roque/integration/login/LoginCucumberTest.java
@@ -10,7 +10,8 @@ import io.cucumber.junit.CucumberOptions;
 		features = { "src/test/resources/login" },
         glue = { "br.com.roque.integration.login" }, 
 //		tags = { "@teste_login_git"},
-		plugin = { "pretty", "json:target/report.json", "de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber" },
+//		plugin = { "pretty", "json:target/report.json", "de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber" },
+		plugin = { "pretty", "json:target/report.json", "cucumber-reporting:target/pretty-cucumber" },
 		stepNotifications = true,
 		monochrome = true
 		)
